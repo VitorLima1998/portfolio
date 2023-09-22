e30e0b0952401441f3917d72152fac6c66f78c81
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-22 18:01:32 -0400

    feat: add home component

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/header';
+import Footer from '../components/footer';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <h1>Home is here</h1>
+      <Footer />
+    </div>
+  );
+}
