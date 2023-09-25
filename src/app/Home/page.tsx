0371e84c285f13e0893911a347cceb952dfcf101
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-25 12:12:46 -0400

    feat: add card component

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,12 +1,14 @@
 import Header from '../components/header';
 import Footer from '../components/footer';
 import Body from '../components/body';
+import Card from '../components/card';
 
 export default function Home() {
   return (
     <div className='py-5 justify-between'>
       <Header />
       <Body />
+      <Card />
       <Footer />
     </div>
   );
