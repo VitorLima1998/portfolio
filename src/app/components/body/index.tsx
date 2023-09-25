d40badbed7fad0e2d844d0d9d1efdcaead79a619
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-25 17:20:13 -0400

    style: add color in baackground

--- src/app/components/body/index.tsx
+++ src/app/components/body/index.tsx
@@ -5,19 +5,21 @@ export default function Body() {
   return (
     <div className='container flex mx-auto justify-center'>
       <div className='max-w-2xl p-3 w-1/2 flex flex-col gap-10 justify-center'>
-        <h1 className='text-7xl font-bold text-justify'>Olá, meu nome é Vitor Lima</h1>
+        <h1 className='text-7xl font-bold text-justify'>
+          Olá, meu nome é Vitor Lima
+        </h1>
         <p className='text-2xl w-full text-justify'>
           Um apaixonado por tecnologia e programação que busca aprimorar os
           conhecimentos para se tornar um dev FullStack.
         </p>
         <div className=' flex w-full gap-4'>
-          <div className='box1 flex items-center border rounded-full px-8 py-2 gap-1 text-black bg-white'>
+          <div className='box1 flex items-center border border-gray-500 rounded-full px-8 py-2 gap-1 text-black bg-white'>
             <FaNodeJs /> BackEnd
           </div>
-          <div className='box1 flex items-center border rounded-full px-8 py-2 gap-1 text-black bg-white'>
+          <div className='box1 flex items-center border border-gray-500 rounded-full px-8 py-2 gap-1 text-black bg-white'>
             <SiTypescript /> FrontEnd
           </div>
-          <div className='box1 flex items-center border rounded-full px-8 py-2 gap-1 text-black bg-white'>
+          <div className='box1 flex items-center border border-gray-500 rounded-full px-8 py-2 gap-1 text-black bg-white'>
             <FaDocker /> DevOps
           </div>
         </div>
