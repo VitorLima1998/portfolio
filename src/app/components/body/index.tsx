a933a4b94ba9faf9e08efd336bb1b86f026fc44b
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-25 12:11:32 -0400

    refactor: change text english to portuguese

--- src/app/components/body/index.tsx
+++ src/app/components/body/index.tsx
@@ -1,9 +1,26 @@
+import { FaNodeJs, FaDocker } from 'react-icons/fa6';
+import { SiTypescript } from 'react-icons/si';
+
 export default function Body() {
   return (
-    <div className='container flex py-5 mx-auto justify-center'>
-      <div className='max-w-2xl p-3 flex flex-col gap-5'>
-        <h1 className='text-7xl'>Hello, my name is Vitor Lima</h1>
-        <p className='text-2xl'> I am a FullStack developer </p>
+    <div className='container flex py-5 mx-auto justify-center h-full gap-10'>
+      <div className='max-w-2xl p-3 flex flex-col gap-10 justify-center'>
+        <h1 className='text-7xl text-justify'>Olá, meu nome é Vitor Lima</h1>
+        <p className='text-2xl w-full text-justify'>
+          Um apaixonado por tecnologia e programação que busca aprimorar os
+          conhecimentos para se tornar um dev FullStack.
+        </p>
+        <div className=' flex w-full gap-4'>
+          <div className='box1 flex items-center border rounded-full px-8 py-2 gap-1 text-black bg-white'>
+            <FaNodeJs /> BackEnd
+          </div>
+          <div className='box1 flex items-center border rounded-full px-8 py-2 gap-1 text-black bg-white'>
+            <SiTypescript /> FrontEnd
+          </div>
+          <div className='box1 flex items-center border rounded-full px-8 py-2 gap-1 text-black bg-white'>
+            <FaDocker /> DevOps
+          </div>
+        </div>
       </div>
       <div>
         <img src='luffy.jpg' alt='Luffy' className='max-h-full rounded-xl' />
