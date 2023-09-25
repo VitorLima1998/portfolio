7c26a38397e8286fcc2e8027599019144a0170bf
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-25 12:12:37 -0400

    styles: change icon

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,13 +1,14 @@
 import Button from '../button';
-import { BsCodeSlash, BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs';
+import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs';
+import { BiCodeAlt } from 'react-icons/bi';
 
 export default function Header() {
   return (
-    <div className='bg-transparent p-3 mx-auto'>
+    <div className='bg-transparent px-10 py-3 mx-auto'>
       <header className='text-center flex justify-between items-center'>
         <div>
           <p className='flex gap-2 items-center text-2xl'>
-            Vitor <BsCodeSlash />
+            Vitor <BiCodeAlt />
           </p>
         </div>
         <div className='flex justify-center space-x-4'>
