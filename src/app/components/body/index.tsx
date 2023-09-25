734fa1ed87547cbfacfeddd0d128bd96dd4de2ab
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-25 15:17:06 -0400

    style: change border

--- src/app/components/body/index.tsx
+++ src/app/components/body/index.tsx
@@ -5,7 +5,7 @@ export default function Body() {
   return (
     <div className='container flex mx-auto justify-center'>
       <div className='max-w-2xl p-3 w-1/2 flex flex-col gap-10 justify-center'>
-        <h1 className='text-7xl text-justify'>Olá, meu nome é Vitor Lima</h1>
+        <h1 className='text-7xl font-bold text-justify'>Olá, meu nome é Vitor Lima</h1>
         <p className='text-2xl w-full text-justify'>
           Um apaixonado por tecnologia e programação que busca aprimorar os
           conhecimentos para se tornar um dev FullStack.
@@ -23,7 +23,7 @@ export default function Body() {
         </div>
       </div>
       <div className='w-1/2 flex justify-center items-center'>
-        <img src='Vitor.jpeg' alt='Vitor' className='h-3/4  rounded-xl' />
+        <img src='Vitor.jpeg' alt='Vitor' className='h-3/4  rounded-3xl' />
       </div>
     </div>
   );
