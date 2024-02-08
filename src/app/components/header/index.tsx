548b2a292c0fc36a440ac457bee7f0313685a9cb
VitorLima1998 <vitor.xlr8@gmail.com>
2024-02-08 15:48:45 -0400

    style: update header styling and layout

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -6,8 +6,8 @@ import ThemeSwitcher from '../../components/toggle/ThemeSwitcher';
 
 export default function Header() {
   return (
-    <div className='bg-transparent px-10 py-3 mx-auto w-full'>
-      <header className='text-center flex justify-between items-center'>
+    <div className='bg-transparent px-5 py-3 mx-auto w-full'>
+      <header className='text-center flex justify-between items-center w-full'>
         <div>
           <p className='flex gap-2 items-center text-2xl'>
             Vitor <BiCodeAlt />
@@ -24,7 +24,7 @@ export default function Header() {
             <Button text={'Sobre'} />
           </Link>
         </div>
-        <div className='flex gap-4 items-center text-2xl '>
+        <div className='flex gap-4 items-center text-2xl'>
           <ThemeSwitcher />
 
           <a
@@ -55,4 +55,3 @@ export default function Header() {
     </div>
   );
 }
-
