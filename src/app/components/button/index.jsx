79e53bffdb858222d52c1bd455a423ace4331b81
VitorLima1998 <vitor.xlr8@gmail.com>
2024-02-08 15:31:41 -0400

    style: update button border color in index.jsx

--- src/app/components/button/index.jsx
+++ src/app/components/button/index.jsx
@@ -1,7 +1,7 @@
 export default function Button(props) {
   return (
     <div>
-      <button className='flex items-center gap-2 bg-transparent border hover:bg-gray-500 font-bold py-2 px-4 rounded-full'>
+      <button className='flex items-center gap-2 bg-transparent border border-gray-500 hover:bg-gray-500 font-bold py-2 px-4 rounded-full'>
         {props.text}
         {props.icon}
       </button>
