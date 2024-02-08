5c1126377eaf4175b0122199e00e72b8e834199a
VitorLima1998 <vitor.xlr8@gmail.com>
2024-02-08 15:30:55 -0400

    refactor: fix footer component layout

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,9 +1,10 @@
-import { SiNextdotjs } from 'react-icons/si';
 import { FaGithub } from 'react-icons/fa6';
+import { SiNextdotjs } from 'react-icons/si';
+
 export default function Footer() {
   return (
-    <div className='bg-transparent px-10 py-3 pt-20 mx-auto'>
-      <footer className='flex justify-between'>
+    <div className='bg-transparent px-10 py-3 pt-20 mx-auto w-full'>
+      <footer className='flex space-x-10 justify-between'>
         <div className='flex gap-5'>
           <p className='flex items-center gap-2'>
             Desenvolvido por Vitor Lima <SiNextdotjs />
