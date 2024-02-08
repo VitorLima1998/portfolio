8c0fd0df702418b48ff2f677452a18e38729b0f6
VitorLima1998 <vitor.xlr8@gmail.com>
2024-02-08 15:32:11 -0400

    refactor: update body component layout and alignment

--- src/app/components/body/index.tsx
+++ src/app/components/body/index.tsx
@@ -3,12 +3,12 @@ import { SiTypescript } from 'react-icons/si';
 
 export default function Body() {
   return (
-    <div className='container flex mx-auto justify-center'>
-      <div className='max-w-2xl p-3 w-1/2 flex flex-col gap-10 justify-center'>
-        <h1 className='text-7xl font-bold text-justify'>
+    <div className='container gap-10 flex flex-col md:flex-row mx-auto justify-center items-center'>
+      <div className='max-w-2xl p-3 w-full md:w-1/2 flex flex-col gap-10 justify-center items-center md:items-start'>
+        <h1 className='text-7xl font-bold text-left md:text-justify'>
           Olá, meu nome é Vitor Lima
         </h1>
-        <p className='text-2xl w-full text-justify'>
+        <p className='text-2xl w-full text-left md:text-justify'>
           Um apaixonado por tecnologia e programação que busca aprimorar os
           conhecimentos para se tornar um dev FullStack.
         </p>
@@ -24,8 +24,12 @@ export default function Body() {
           </div>
         </div>
       </div>
-      <div className='w-1/2 flex justify-center items-center'>
-        <img src='Vitor.jpeg' alt='Vitor' className='h-3/4  rounded-3xl' />
+      <div className='w-full md:w-1/2 flex justify-center items-center'>
+        <img
+          src='Vitor.jpeg'
+          alt='Vitor'
+          className='mx-auto h-3/4 rounded-3xl'
+        />
       </div>
     </div>
   );
